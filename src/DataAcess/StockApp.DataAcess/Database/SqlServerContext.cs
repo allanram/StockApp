51b7dfafe54a6383ccf7e09d2932acaf5975dca5
allan <allanramosdeaguiar805@gmail.com>
2023-12-01 22:00:26 -0300

    feat: Adicionado

--- src/DataAcess/StockApp.DataAcess/Database/SqlServerContext.cs
+++ src/DataAcess/StockApp.DataAcess/Database/SqlServerContext.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace StockApp.DataAcess.Database
 {
-    internal class SqlServerContext
+    internal static class SqlServerContext
     {
+      
+        
+            /// <summary>
+            /// Propriedade <c>Conexao</c> retorna a string que foi definida no arquivo App.config.
+            /// </summary>
+
+
+            internal static string ConexaoSemBanco => @"Data Source=.\SQLEXPRESS;User ID=sa;Password=sql2022; Trusted_Connection=False; TrustServerCertificate=True";
+           internal static string ConexaoComBanco => @"Data Source=.\SQLEXPRESS;Initial Catalog=STOCKELETRO;User ID=sa;Password=sql2022; Trusted_Connection=False; TrustServerCertificate=True";
+
+
+
     }
 }
